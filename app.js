9fd56031a65211db8ba048d4e6f14ab463d96e1b
Subhankar <s1panda@ucsd.edu>
2017-01-26 20:28:47 -0800

    checkpoint

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var app = express();
 var path = require('path');
 
 var index = require("./routes/index");
+var code = require("./routes/code");
 // Set the view engine
 app.set('view engine', 'jade');
 
@@ -13,15 +14,23 @@ app.set('views', __dirname + '/views');
 app.use('/', index);
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/resume', function(req, res){
+	console.log(__dirname);
+	res.sendFile(__dirname + "/public/assets/Subhankar_Panda_Resume.pdf");
+});
+
+app.use('/code', code)
+
 app.use(function(req, res, next){
   res.status(404).send('page not found');
 });
+
 
 // Create HTTP server with your app
 var http = require("http");
 var server = http.createServer(app)
 
 // Listen to port 3000 
-server.listen(3000);
+app.listen(3000);
 console.log("port 3000!");
 
